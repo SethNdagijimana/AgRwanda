0bf707d556ee6345aa6b37f575d7f08f48b93527
SethNdagijimana <sethreas@gmail.com>
2022-08-25 21:07:35 +0200

    updates

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
-// import Content from '../navbar/Content';
-// import background from "../../assets/image.png";
 import Navbar from '../navbar/Navbar';
-import { useState } from "react";
-
 import "./home.css";
+import ImageSlider from './ImageSlider';
+import { sliderData } from './ImageSliderData';
 
 function Home() {
-
-    const [currentSlide,setCurrentSlide] = useState(0);
-
-    const data = [
-        {
-            id: "1",
-        }
-    ]
-
-
-
-
-
   return (
-    <div className="content">
-       <Navbar />
-       {/* <Content /> */}
-       {/* <h2>The purpose of Agriculture Rwanda Platform is to provide easy access to agricultural products to the local farmers.</h2>
-       <img src={background} alt="bg" className="background"></img> */}
-
-      
-
+    <div className="container">
+        <Navbar />
+        <ImageSlider slides={sliderData} />
 
     </div>
   )
---
+++ src/components/home/ImageSliderData.js
@@ -0,0 +1,13 @@
+export const sliderData = [
+
+    {
+        image: "../../assets/image.png"
+    },
+    {
+        image: "../../assets/PEST 1.png"
+    },
+    {
+        image: "../../assets/pexels-mark-stebnicki-2252584 1.png"
+    },
+   
+];
