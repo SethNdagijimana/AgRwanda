df750d8b7fdae5a8c06be455197131ec50d0ba42
Huguette Tunga <tunganyiligirah@gmail.com>
2022-08-25 21:51:56 +0200

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './App.css';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import Home from "./components/home/Home";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 function App() {
   return (
-    <div className="app">
+
+     <BrowserRouter>
+     <Routes>
+      <Route element={<Home />}path='/'/>
+      <Route element={<AboutUs/>} path='/AboutUs'/>
+     </Routes>
+     </BrowserRouter>
     
-    <Home />
-      
-    </div>
   );
 }
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
 // import Content from '../navbar/Content';
-// import background from "../../assets/image.png";
+import background from "../../assets/image.png";
 import Navbar from '../navbar/Navbar';
-import { useState } from "react";
 
 import "./home.css";
 
 function Home() {
-
-    const [currentSlide,setCurrentSlide] = useState(0);
-
-    const data = [
-        {
-            id: "1",
-        }
-    ]
-
-
-
-
-
   return (
     <div className="content">
        <Navbar />
+       
        {/* <Content /> */}
-       {/* <h2>The purpose of Agriculture Rwanda Platform is to provide easy access to agricultural products to the local farmers.</h2>
-       <img src={background} alt="bg" className="background"></img> */}
+       <h2>The purpose of Agriculture Rwanda Platform is to provide easy access to agricultural products to the local farmers.</h2>
+       <img src={background} alt="bg" className="background"></img>
 
       
 
