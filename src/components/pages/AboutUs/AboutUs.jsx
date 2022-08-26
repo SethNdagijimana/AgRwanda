d264fd554aa80ee7fa84910cef9eacc32d3d69ab
Huguette Tunga <tunganyiligirah@gmail.com>
2022-08-26 12:42:00 +0200

    About changes

--- src/components/pages/AboutUs/AboutUs.jsx
+++ src/components/pages/AboutUs/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AboutUs.css';
+import { Link } from 'react-router-dom'
 import Navbar from '../../navbar/Navbar';
 import bg from "../../../assets/Bg-Abt.png";
 
@@ -8,6 +9,14 @@ function AboutUs() {
     <div>
         <Navbar/>
         <img src={bg}alt=''/>
+        <div className='log-cont'>
+        <div className='Abt-tle'>Join us</div>
+        <div className='Abt-cont'> we provide easy access
+        to agricultural products 
+        to local farmers.</div>
+        <button className='sign'>Sign up</button>
+        <Link to='/login'><button className='log'>Login</button></Link>
+        </div>
     </div>
   )
 }
