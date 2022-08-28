aac7c6b314f5ec0f633ba3d0bca04c22092cb37c
Huguette Tunga <tunganyiligirah@gmail.com>
2022-08-28 16:26:28 +0200

    Login changes

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import "./login.css";
+// import LoginB from '../../buttons/LoginB'
+import Navbar from '../../navbar/Navbar';
+import bgreen from "../../../assets/PartGreen.png";
+import bgreenI from "../../../assets/InGreen.png";
+import { Link } from 'react-router-dom'
 
 
 function Login() {
   return (
     <div>
-        <h1>Login</h1>
+    <Navbar/>
+    <img src={bgreen}alt='' className='PartGreen'/>
+    <img src={bgreenI}alt='' className='InGreen'/>
+    <p className='Log_in'>Log in</p>
+    <input type={"text"} placeholder="Email" className='email'/>
+    <input type={'password'} placeholder="Password" className='password'/>
+    <Link to='/login'><button className='log'>Login</button></Link>
+    <Link to='/recoverPassword'><button className='RecPass'>Forgot Password</button></Link>
+    {/* <LoginB id="logx"/> */}
+
     </div>
   )
 }
---
+++ src/components/pages/recPassword/RecPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecPassword() {
+  return (
+    <div>RecPassword</div>
+  )
+}
+
+export default RecPassword
