f00627e32e9c3922052d706d0695aa1a9cc8cc35
SethNdagijimana <sethreas@gmail.com>
2022-08-28 08:23:05 +0200

    commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,16 +3,12 @@ import Content from '../navbar/Content';
 // import background from "../../assets/image.png";
 import Navbar from '../navbar/Navbar';
 import Card from '../navbar/card/Card';
-
-
 import "./home.css";
 import ImageSlider from './ImageSlider';
 import { sliderData } from './ImageSliderData';
 
 function Home() {
   return (
-
-  
 
       
 
