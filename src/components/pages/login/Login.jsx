f243b12ec4cceaae0c48335dbb57151152c2205e
SethNdagijimana <sethreas@gmail.com>
2022-08-31 11:12:36 +0200

    changing log in

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -5,6 +5,7 @@ import Navbar from '../../navbar/Navbar';
 import bgreen from "../../../assets/PartGreen.png";
 import bgreenI from "../../../assets/InGreen.png";
 import { Link } from 'react-router-dom'
+import * as AiIcons from "react-icons/ai"; 
 
 
 function Login() {
@@ -13,12 +14,18 @@ function Login() {
     <Navbar/>
     <img src={bgreen}alt='' className='PartGreen'/>
     <img src={bgreenI}alt='' className='InGreen'/>
-    <p className='Log_in'>Log in</p>
-    <input type={"text"} placeholder="Email" className='email'/>
-    <input type={'password'} placeholder="Password" className='password'/>
+    <form className='form'> 
+         <p className='Log_in'>Log in</p>
+         <div className="login-form">
+    <input type="text" placeholder="Email" className='emailx' />
+    <AiIcons.AiOutlineMail />
+    <input type='password' placeholder="Password" className='password'/>
     <Link to='/login'><button className='log'>Login</button></Link>
     <Link to='/recoverPassword'><button className='RecPass'>Forgot Password</button></Link>
     {/* <LoginB id="logx"/> */}
+    </div>
+    </form>
+
 
     </div>
   )
