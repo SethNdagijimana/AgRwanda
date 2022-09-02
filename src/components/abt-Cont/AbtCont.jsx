6e2f34f97b9218b39cacb6f214dc54ec1b06b6b2
Huguette Tunga <tunganyiligirah@gmail.com>
2022-09-02 08:13:00 +0200

    subAbout changes

---
+++ src/components/abt-Cont/AbtCont.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './abtCont.css'
+
+const AbtCont= (props) => {
+  return (
+    <div className='Dserv'>
+          <div className='product_details'>
+            <h className='h-Ttl'>{props.title}</h>
+            <p className='p-Cont'>{props.content}</p>
+            
+          </div>
+          </div>
+  )
+}
+
+export default AbtCont
--- src/components/pages/AboutUs/AboutUs.jsx
+++ src/components/pages/AboutUs/AboutUs.jsx
@@ -3,6 +3,7 @@ import './AboutUs.css';
 import { Link } from 'react-router-dom'
 import Navbar from '../../navbar/Navbar';
 import bg from "../../../assets/Bg-Abt.png";
+import AbtCont from '../../abt-Cont/AbtCont';
 
 function AboutUs() {
   return (
@@ -18,10 +19,24 @@ function AboutUs() {
         <div className="btn">
         <Link to="/signup"><button className='sign'>Sign up</button></Link>
         <Link to='/login'><button className='login'>Login</button></Link>
-
         </div>
-
-
+        
+          <p className='serv'>OUR SERVICES</p>
+          <div className="abt_container">
+          <div className='Abt-2'>
+          <AbtCont
+          title = 'Fertilisers'
+          content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ac tristique id massa. Curabitur commodo pellentesque amet, feugiat. Duis malesuada enim non, pulvinar diam lorem eget et. Urna, in laoreet nisi commodo ultricies. Gravida faucibus pharetra, enim at platea enim nunc massa. Interdum lectus donec facilisis nunc elit semper id volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ac tristique id massa. Curabitur commodo pellentesque amet, feugiat. Duis malesuada enim non, pulvinar diam lorem eget et. Urna, in laoreet nisi commodo ultricies. Gravida faucibus pharetra, enim at platea enim nunc massa. Interdum lectus donec facilisis nunc elit semper id volutpat."
+          />
+          </div>
+          <div className="abt-3">
+          <AbtCont 
+          title = 'Seeds'
+          content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ac tristique id massa. Curabitur commodo pellentesque amet, feugiat. Duis malesuada enim non, pulvinar diam lorem eget et. Urna, in laoreet nisi commodo ultricies. Gravida faucibus pharetra, enim at platea enim nunc massa. Interdum lectus donec facilisis nunc elit semper id volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ac tristique id massa. Curabitur commodo pellentesque amet, feugiat. Duis malesuada enim non, pulvinar diam lorem eget et. Urna, in laoreet nisi commodo ultricies. Gravida faucibus pharetra, enim at platea enim nunc massa. Interdum lectus donec facilisis nunc elit semper id volutpat.'
+          />
+          </div>
+          </div>
+          
         </div>
     </div>
   )
